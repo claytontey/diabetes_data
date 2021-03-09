211c0ac3183c6edcc8ee2bbcc79862af38d52dbd
Clayton <claytontey@gmail.com>
2021-03-09 04:17:46 -0300

    upgrade

--- src/kaggle_diabetes.py
+++ src/kaggle_diabetes.py
@@ -1,8 +1,15 @@
 import pandas as pd
-import numpy as numpy
+import numpy as np
 import os
+import time
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB 
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVC
+plt.style.use('ggplot')
 
 
 
@@ -28,6 +35,13 @@ df['diabetes'] = df['diabetes'].map(map_data)
 vl_paciente = len(df.loc[df['diabetes'] == 1])
 vl_controle = len(df.loc[df['diabetes'] == 0])
 
+# Creates a list of training time, predicting time and accuracies
+train_time_NB, predict_time_NB, accuracy_NB = [], [], []
+train_time_DT, predict_time_DT, accuracy_DT = [], [],[]
+train_time_SVM, predict_time_SVM, accuracy_SVM = [],[],[]
+
+
+
 # **************************** Funções ***************************************
 
 def information():
@@ -38,11 +52,107 @@ def information():
     print('\nSamples and Features:\n')
     print(df.head())
     print('\nO conjunto de dados possui %d linhas e %d colunas para: ' %(len(df[:]), len(df.columns)))
-    #print('\nDiagnóstico por classe:\n', df['diabetes'].value_counts())
     print('     %d pacientes, que correspondem a %.2f%% do conjunto de dados' %(vl_paciente, vl_paciente / (vl_paciente + vl_controle)*100))
     print('     %d controles, que correspondem a %.2f%% do conjunto de dados' %(vl_controle, vl_controle / (vl_controle + vl_paciente)*100))
-    #print('Valores faltantes: ', df.isnull().values.any())
+    print('\nValores faltantes: ', df.isnull().values.any())
+    print('\nValor de correlação: ',df.corr())
+
+def formatarTimer(start,end):
+    hours, rem = divmod(end-start, 3600)
+    minutes, seconds = divmod(rem, 60)
+    return seconds
+
+
+
+# divisão entre as características e as classes a serem preditas
+def split_model():
+    print('\n**************** Resultados **********************************\n')
+    dt_feature = df.iloc[:,:-1]
+    dt_target = df.iloc[:,-1]
+
+    rould = 0.10
+    epochs = 1
+    # Iterates over a set of runnings
+    for i in range(9):
+        X_train, X_test, y_train, y_test = train_test_split(dt_feature, dt_target, test_size=0.3, random_state=i)
+        
+
+        # Naive Bayes
+        start = time.time()
+        gnb = GaussianNB()
+        gnb.fit(X_train, y_train)
+        train_time_NB.append(formatarTimer(start, time.time()))
+        start = time.time()
+        gnb_predictions = gnb.predict(X_test)
+        predict_time_NB.append(formatarTimer(start, time.time()))
+        acc_nb = gnb.score(X_test, y_test)
+        
+        
+        
+        # Decision Tree
+        start = time.time()
+        dt = DecisionTreeClassifier(random_state=i, criterion='entropy', max_depth=3)
+        dt.fit(X_train, y_train)
+        train_time_DT.append(formatarTimer(start, time.time()))
+        start = time.time()
+        dt_predictions = dt.predict(X_test)
+        predict_time_DT.append([formatarTimer(start, time.time())])
+        acc_dt = dt.score(X_test, y_test)
+        
+        
+        # SVM-RBF
+        start = time.time()
+        param_grid = {'C':[1, 5, 10, 50, 100, 500, 1000, 5000], 'gamma':[0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1]}
+        rbf = GridSearchCV(SVC(kernel='rbf', class_weight='balanced'), param_grid)
+        rbf = rbf.fit(X_train, y_train)
+        #rbf = svm.SVC(kernel='rbf', gamma=1, C=1, decision_function_shape='ovo')
+        #rbf.fit(X_train, Y_train)
+        train_time_SVM.append(formatarTimer(start, time.time()))
+        start = time.time()
+        rbf_pred = rbf.predict(X_test)
+        predict_time_SVM.append(formatarTimer(start, time.time()))
+        acc_rbf = rbf.score(X_test, y_test)
+        print('Best estimator found by grid search: ')
+        print(rbf.best_estimator_)
+        print('Best Score: ',rbf.best_score_)
+        
+        # Accuracy
+        accuracy_NB.append(acc_nb)
+        accuracy_DT.append(acc_dt)
+        accuracy_SVM.append(acc_rbf)
+        
+         
+        print('\nResultados Nayve Bayes:\nAcc_NB: ',acc_nb,'\nepoch: ',epochs,'\ntempo de treinamento: ',train_time_NB)
+        print('\nResultados Decision Tree:\nAcc_DT: ',acc_dt,'\nepoch: ',epochs,'\ntempo de treinamento: ',train_time_DT)
+        print('\nResultados Suport Vector Machine:\nAcc_SVM: ',acc_rbf,'\nepoch: ',epochs,'\ntempo de treinamento: ',train_time_SVM)    
+        print('\n\n')
+        rould+=0.10
+        epochs+=1
+
+
+    print('Acuracia média Nayve Bayes de %.2f%% com tempo médio de %.5f segundos para treinar.' \
+        %(np.mean(accuracy_NB)*100, np.mean(train_time_NB)))
+    print('Acuracia média Decision Tree de %.2f%% com tempo médio de %.5f segundos para treinar.' \
+        %(np.mean(accuracy_DT)*100, np.mean(train_time_DT)))
+    print('Acuracia média Suport Vector Machine de %.2f%% com tempo médio de %.5f segundos para treinar.' \
+        %(np.mean(accuracy_SVM)*100, np.mean(train_time_SVM)))
     
+
+# Feature selection
+def feat_select():
+    model = LogisticRegression(max_iter=2000)
+    from sklearn.feature_selection import RFE
+    rfe = RFE(model, 4)
+    fit = rfe.fit(dt_feature, dt_target)
+    # Mostrando o número de features:
+    print ("Número de features = %d"%(fit.n_features_))  
+
+
+# **************************** Plots ***************************************
+def correlation():
+    print('Matriz de correlação: \n')
+    plot_corr(df)
+    plt.show()
 
 def plot_corr(df, size=10):
     corr = df.corr()
@@ -50,18 +160,13 @@ def plot_corr(df, size=10):
     ax.matshow(corr)
     plt.xticks(range(len(corr.columns)), corr.columns)
     plt.yticks(range(len(corr.columns)), corr.columns)
-
-
-
-# **************************** Plots ***************************************
-def correlation():
-    print('Valor de correlação: \n')
-    df.corr()
-
-    print('Matriz de correlação: \n')
-    plot_corr(df)
     plt.show()
 
 
+
+
+#plot_corr(df, size=10)
 information()
 #correlation()
+#split_model()
+#feat_select()
